1e6bdb157b03d58d667a183faeddaf585c8cb304
Ivar Karstensen <pricelessdev@gmail.com>
2024-12-13 01:26:42 +0100

    Initial commit

---
+++ api/src/main/java/me/albusthepenguin/api/APIExample.java
@@ -0,0 +1,6 @@
+package me.albusthepenguin.api; // Ensure it's in the right package
+
+public class APIExample {
+    // A simple static variable we want to access in the core module
+    public static String exampleMessage = "Hello from the API module!";
+}
---
+++ core/src/main/java/me/albusthepenguin/puppetMaster/PuppetMaster.java
@@ -0,0 +1,17 @@
+package me.albusthepenguin.puppetMaster;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PuppetMaster extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
